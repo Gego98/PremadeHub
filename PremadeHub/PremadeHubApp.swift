94b90f7d08800a078219654b1488d1dd6e84d72a
Filip Gegovski <f.gegovski@gmail.com>
2025-12-25 23:50:19 +0100

    Initial Commit

---
+++ PremadeHub/PremadeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremadeHubApp.swift
+//  PremadeHub
+//
+//  Created by Filip Gegovski on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PremadeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
